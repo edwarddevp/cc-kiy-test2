4c3a95a0a1454c5279146f8af5d879e54e919aa5
code.developer13 <dev13@codecraft.com>
2020-06-04 14:24:25 -0400

    some fixes

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -9,7 +9,7 @@ import Modal  from "@cc-test2/modal"
 import NavBar  from "@cc-test2/navbar"
 import PageHeader  from "@cc-test2/pageheader"
 import Paginate  from "@cc-test2/paginate"
-import Sidebar  from "@cc-test2/sidebar"
+import SideBar  from "@cc-test2/sidebar"
 import PhotoGridAndAction from "@cc-test2/photogridandaction";
 
 export {
@@ -24,6 +24,6 @@ export {
   NavBar,
   PageHeader,
   Paginate,
-  Sidebar,
+  SideBar,
   PhotoGridAndAction
 }
